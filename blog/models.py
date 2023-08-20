a339fcb4c58e018f0c8096a048fda4a4a0ec62bc
BenHowkins <>
2023-08-20 11:47:04 +0000

    Created Post and Comment admin filtering and comment approval function

--- blog/models.py
+++ blog/models.py
@@ -29,6 +29,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
     name = models.CharField(max_length=100)
+    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
